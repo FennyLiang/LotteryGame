72f5d7e8f2703962c85f692bcc47780744b37e04
FennyLiang <shras1993@gmail.com>
2016-12-10 23:20:15 +0800

    add color changed method

--- js/random.js
+++ js/random.js
@@ -2,16 +2,33 @@ var nums = [];
 
 $('#start').on('click', function(){
 
-	var random = Math.floor(Math.random()*16);
+	var random = Math.floor((Math.random()*16) + 1);
   		console.log(random);
   		if(nums.indexOf(random) < 0){
   			nums.push(random);
-  			$('#values').append('<li>'+random+'</li>')
   		}
 
-  		if($('#values').children().length > 4){
-  			$('#values').children().remove();
-  			nums = [];
+  		if(nums.length > 4){
+  			return nums;
+  			// $('#values').append('<li>'+random+'</li>')
   		}
+
+  		nums.forEach(changeColor);
+
+  		function changeColor(element, index, array){
+  			if( index != 3){
+  				$('#'+element).css("color","orange")
+  			}
+
+  			else{
+  				$('#'+element).css("color","red")
+  			}
+  		}
+
+
+  		// if($('#values').children().length > 4){
+  		// 	$('#values').children().remove();
+  		// 	nums = [];
+  		// }
 
 })
