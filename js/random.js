983364981cb0c69fae81a91d6a180db1c29c0557
FennyLiang <shras1993@gmail.com>
2016-12-11 23:51:06 +0800

    add change color / show history method

--- js/random.js
+++ js/random.js
@@ -1,34 +1,52 @@
 var nums = [];
 
 $('#start').on('click', function(){
-
-	var random = Math.floor((Math.random()*16) + 1);
+	
+		var random = Math.floor((Math.random()*16) + 1);
   		console.log(random);
-  		if(nums.indexOf(random) < 0){
-  			nums.push(random);
-  		}
 
-  		if(nums.length > 4){
-  			return nums;
-  			// $('#values').append('<li>'+random+'</li>')
-  		}
+		if(nums.indexOf(random) < 0 && nums.length < 4){
+
+			nums.push(random);
+		}
 
   		nums.forEach(changeColor);
-
-  		function changeColor(element, index, array){
-  			if( index != 3){
-  				$('#'+element).css("color","orange")
-  			}
-
-  			else{
-  				$('#'+element).css("color","red")
-  			}
-  		}
+ 		
+ 		
+})
 
 
-  		// if($('#values').children().length > 4){
-  		// 	$('#values').children().remove();
-  		// 	nums = [];
-  		// }
+function showHistory(nums){
+	$('#history').append('<li>'+nums+'</li>');
+	$('#start').hide();
+	$('#reset').show();
+	return nums;
+}
+
+$('#reset').on('click', function(){
+	resetArray(nums);
+	$('#start').show();
+	$('#reset').hide();
 
 })
+
+function resetArray(nums){
+	nums.forEach((element, index, array) =>{
+		$('#'+element).css("color", "black")
+	})
+	nums.length = 0;
+}
+
+
+function changeColor(element, index, array){
+	if( index != 3){
+		$('#'+element).css("color", "orange")
+	}
+
+	else{
+		$('#'+element).css("color", "red")
+		showHistory(array);
+}
+	}
+
+
