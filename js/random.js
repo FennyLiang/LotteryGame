2f1f59bdabc62dc4ed5f36f9815de1c0ba7c5fd9
FennyLiang <shras1993@gmail.com>
2016-12-13 15:25:04 +0800

    fix coding style

--- js/random.js
+++ js/random.js
@@ -1,62 +1,75 @@
-var nums = [];
+var nums = []
+var count = 0
 
 $('#start').on('click', function(){
 	
-	var random = Math.floor((Math.random()*16) + 1);
-		// console.log(random);
+	var random = Math.floor((Math.random()*16) + 1)
+	console.log(random)
 
 	if(nums.indexOf(random) < 0 && nums.length < 4){
 
-		nums.push(random);
+		nums.push(random)
 	}
 
-		nums.forEach(changeColor);
+	nums.forEach(changeColor)
  		
  		
 })
 
-var count = 0;
 
-function showHistory(nums){
-	$('#history').prepend('<li>'+nums+'</li>');
-	$('#start').hide();
-	$('#reset').show();
+
+function showHistory (nums) {
+	$('#history').prepend('<li>'+nums+'</li>')
+
+	$('#start').hide()
+	$('#reset').show()
+
 	count ++;
-	console.log(count);
-	if( count > 5 ){
-		removeData();
+	console.log(count)
+
+	if( count > 5 ) {
+
+		removeData()
 	}
 }
 
-function removeData(){
+function removeData () {
 
-	$('#history li:last-child').remove();
+	$('#history li:last-child').remove()
 
 }
 
-$('#reset').on('click', function(){
-	resetArray(nums);
-	$('#start').show();
-	$('#reset').hide();
+$('#reset').on('click', function() {
+
+	resetArray(nums)
+
+	$('#start').show()
+	$('#reset').hide()
 
 })
 
-function resetArray(nums){
-	nums.forEach((element, index, array) =>{
-		$('#'+element).css("color", "black")
+function resetArray (nums) {
+
+	nums.forEach ((element, index, array) => {
+		$('#'+element).css("color", "white")
 	})
-	nums.length = 0;
+	
+	nums.length = 0
 }
 
 
-function changeColor(element, index, array){
-	if( index != 3){
-		$('#'+element).css("color", "orange")
-	}
+function changeColor (element, index, array) {
 
+	if( index != 3){
+
+		$('#'+element).css("color", "orange")
+
+	}
 	else{
+
 		$('#'+element).css("color", "red")
-		showHistory(array);
+
+		showHistory(array)
 }
 	}
 
